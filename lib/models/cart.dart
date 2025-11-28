13a6db4c886ebe5717598724fcd566c00d88005f
K-agya <149212228+K-agya@users.noreply.github.com>
2025-11-28 15:52:42 +0000

    cart model

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,44 +1,48 @@
-import 'package:sandwich_shop/repositories/pricing_repository.dart';
+import 'package:sandwich_shop/models/sandwich.dart';
 
 class CartItem {
-  final String itemType;
-  final BreadType breadType;
+  final Sandwich sandwich;
   int quantity;
 
   CartItem({
-    required this.itemType,
-    required this.breadType,
+    required this.sandwich,
     this.quantity = 1,
   });
 }
 
 class Cart {
   final List<CartItem> _items = [];
-  final PricingRepository _pricingRepository;
+  final double Function({required int quantity, required bool isFootlong}) calculatePrice;
 
-  Cart(this._pricingRepository);
+  Cart(this.calculatePrice);
 
-  void addItem(String itemType, BreadType breadType) {
+  void add(Sandwich sandwich, {int quantity = 1}) {
     final existingItem = _items.firstWhere(
-      (item) => item.itemType == itemType && item.breadType == breadType,
-      orElse: () => CartItem(itemType: itemType, breadType: breadType),
+      (item) =>
+          item.sandwich.type == sandwich.type &&
+          item.sandwich.isFootlong == sandwich.isFootlong &&
+          item.sandwich.breadType == sandwich.breadType,
+      orElse: () => CartItem(sandwich: sandwich, quantity: 0),
     );
 
     if (existingItem.quantity > 0) {
-      existingItem.quantity++;
+      existingItem.quantity += quantity;
     } else {
-      _items.add(existingItem);
+      _items.add(CartItem(sandwich: sandwich, quantity: quantity));
     }
   }
 
-  void removeItem(String itemType, BreadType breadType) {
+  void remove(Sandwich sandwich, {int quantity = 1}) {
     final existingItem = _items.firstWhere(
-      (item) => item.itemType == itemType && item.breadType == breadType,
-      orElse: () => CartItem(itemType: itemType, breadType: breadType),
+      (item) =>
+          item.sandwich.type == sandwich.type &&
+          item.sandwich.isFootlong == sandwich.isFootlong &&
+          item.sandwich.breadType == sandwich.breadType,
+      orElse: () => CartItem(sandwich: sandwich, quantity: 0),
     );
 
-    if (existingItem.quantity > 1) {
-      existingItem.quantity--;
+    if (existingItem.quantity > quantity) {
+      existingItem.quantity -= quantity;
     } else {
       _items.remove(existingItem);
     }
@@ -47,9 +51,9 @@ class Cart {
   double calculateTotalPrice() {
     double total = 0.0;
     for (var item in _items) {
-      total += _pricingRepository.calculatePrice(
+      total += calculatePrice(
         quantity: item.quantity,
-        isFootlong: item.itemType == 'footlong',
+        isFootlong: item.sandwich.isFootlong,
       );
     }
     return total;
